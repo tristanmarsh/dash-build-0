957c06aae1f3dd35379fbabe65d5374947560113
Michael <mjlai3@student.monash.edu>
2015-04-23 15:07:20 +1000

    CRUD for bloody everything

--- tools/buildzeroseven.sql
+++ tools/buildzeroseven.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2015 at 01:05 PM
+-- Generation Time: Apr 23, 2015 at 07:05 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,96 @@ SET time_zone = "+00:00";
 --
 -- Database: `buildzeroseven`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `emergency_contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_student`
+--
+
+CREATE TABLE IF NOT EXISTS `emergency_student` (
+  `emergency_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  KEY `emergency_id` (`emergency_id`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE IF NOT EXISTS `employees` (
+  `employee_id` int(11) NOT NULL,
+  KEY `employee_id` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `internet_connection`
+--
+
+CREATE TABLE IF NOT EXISTS `internet_connection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lease_id` int(11) NOT NULL,
+  `bandwidth` enum('1GB','5GB','10GB') NOT NULL,
+  `monthly_fee` enum('TEN','TWENTY','FORTY') NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date NOT NULL,
+  `status` enum('ACTIVE','INACTIVE') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lease_id` (`lease_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `internet_connection`
+--
+
+INSERT INTO `internet_connection` (`id`, `lease_id`, `bandwidth`, `monthly_fee`, `date_start`, `date_end`, `status`) VALUES
+(1, 1, '1GB', '', '2015-04-23', '2015-09-23', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leases`
+--
+
+CREATE TABLE IF NOT EXISTS `leases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date NOT NULL,
+  `lease_status` enum('ONGOING','EXPIRED') NOT NULL,
+  `weekly_price` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  KEY `student_id` (`student_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `leases`
+--
+
+INSERT INTO `leases` (`id`, `room_id`, `student_id`, `date_start`, `date_end`, `lease_status`, `weekly_price`) VALUES
+(1, 1, 1, '2015-04-23', '2015-10-23', 'ONGOING', 200);
 
 -- --------------------------------------------------------
 
@@ -52,11 +142,95 @@ INSERT INTO `maintenances` (`id`, `title`, `description`, `created`, `modified`,
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `payments`
+--
+
+CREATE TABLE IF NOT EXISTS `payments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lease_id` int(11) NOT NULL,
+  `date_paid` date NOT NULL,
+  `amount` int(11) NOT NULL,
+  `payment_period_starting` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lease_id` (`lease_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+CREATE TABLE IF NOT EXISTS `properties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `number_rooms` int(11) NOT NULL,
+  `bathrooms` int(11) NOT NULL,
+  `kitchens` int(11) NOT NULL,
+  `storeys` int(11) NOT NULL,
+  `garage` enum('TRUE','FALSE') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`id`, `address`, `number_rooms`, `bathrooms`, `kitchens`, `storeys`, `garage`) VALUES
+(1, '70 something street', 5, 1, 1, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `property_id` int(11) NOT NULL,
+  `vacant` enum('TRUE','FALSE') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `property_id` (`property_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `property_id`, `vacant`) VALUES
+(1, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `expected_grad_date` date NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `country_of_birth` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `person_id` (`person_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `expected_grad_date`, `person_id`, `country_of_birth`) VALUES
+(1, '2015-10-23', 28, 'china'),
+(2, '2015-04-23', 29, 'hong kong');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `role` varchar(20) DEFAULT NULL,
@@ -81,20 +255,57 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `created`, `modified`, `title`, `first_name`, `last_name`, `gender`, `phone`, `email`, `home_country`, `city`, `suburb`, `postcode`) VALUES
-(1, 'admin', '$2y$10$ytpEvi6EvojZvBDYa18lDezDToFfQ9e64lD.gIo0r30z9uscH4xXy', 'admin', NULL, NULL, 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(5, 'tenant1', '$2y$10$36QA3Xx3il4R9MmIrIn59e54Ii.TmrCAJKqezyb.u9na8wx2/0DHS', 'author', '2015-04-13 14:38:23', '2015-04-13 14:38:23', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(6, 'tenant2', '$2y$10$EaSEaQ3sNeSnnvZ0yvtl2Oxh3Vbpwjh29W4bF/.5PD1OHXlNxS2Ga', 'author', '2015-04-13 14:52:52', '2015-04-13 14:52:52', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(7, 'tenant3', '$2y$10$UeVYZ1eJPQ/hxxoEDJeMj.iXrRuT7mLTPNKI9QqxTg1YnSVrT8Gwq', 'author', '2015-04-13 15:03:48', '2015-04-13 15:03:48', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(8, 'test$#', '$2y$10$T/5XhbcP1NDaXQdps5qaQOe1pF0HKaoY7kTgdCOL0q6FvsqoylkBi', 'author', '2015-04-13 15:26:58', '2015-04-13 15:26:58', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(9, 'dasfasdfasdfsad', '$2y$10$dhDXDrUuLqGgPwdUZ329kugptnnNvOm7mv9dd0aw6F7UYJ72G4kim', 'author', '2015-04-13 15:37:29', '2015-04-13 15:37:29', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(10, '12345678', '$2y$10$ODlg8YqFA09N5zFaARNEXO6BeTOG7a9i.o84j4pScuuy5Svs90gVG', 'author', '2015-04-14 03:48:15', '2015-04-15 07:48:23', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(11, 'tenant10', '$2y$10$fYYcpgGYQCeJVTHeP7zh5eC3.26OXUiE6Ihv1sFJ9d4xd57/PCYSm', 'author', '2015-04-14 09:12:01', '2015-04-14 09:12:01', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(22, 'tenant4', '$2y$10$nxhYe/k.RxWKGRcpKtDx1eg0N7d883UpEixXW2Yy/gWcL0aTEaSGS', 'author', '2015-04-15 07:25:27', '2015-04-15 07:25:27', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(24, 'tenant12123123', '$2y$10$/IIvrHEHDjk4ZhzbIS28d.5zp.XvcO5oFISgn.lK6xyXGTty3aC/S', 'author', '2015-04-16 05:11:17', '2015-04-16 05:11:17', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(26, 'tenant123890', '$2y$10$JPmPChO1FvNjETOIzmyZ9.AvW0SgSbyjmlCcd8CGdikyr4l260Zaa', 'author', '2015-04-16 05:25:22', '2015-04-16 05:25:22', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
-(27, 'hello', '$2y$10$PpAe7nhqL4CZGY./.hOUX.1Q2ZDg4sSvSyjSjNPAHSSR0U46RCQoi', 'author', '2015-04-16 05:35:28', '2015-04-16 05:35:28', 'MR', NULL, '', 'M', 0, '', '', '', '', 0),
+(1, 'admin', '$2y$10$ytpEvi6EvojZvBDYa18lDezDToFfQ9e64lD.gIo0r30z9uscH4xXy', 'admin', NULL, NULL, 'MR', 'Tony', 'Wise', 'M', 0, '', '', '', '', 0),
 (28, 'tenant50', '$2y$10$rsTuv7CgtHwhyjtV4BqK9Om2W33vRJKlVs1SZpP/fxeImQt1YXJHK', 'author', '2015-04-22 10:38:52', '2015-04-22 10:38:52', 'MR', 'jacob', '', 'M', 0, '', '', '', '', 0),
 (29, 'tenant51', '$2y$10$T2vn/qK4X/1r7h6QifMMyu229y5zDSSUAakejLXeB.Fhufop.rL5q', 'author', '2015-04-22 10:57:05', '2015-04-22 10:57:05', 'MR', 'mike', 'lai', 'M', 0, '', '', '', '', 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `emergency_student`
+--
+ALTER TABLE `emergency_student`
+  ADD CONSTRAINT `emergency_student_ibfk_1` FOREIGN KEY (`emergency_id`) REFERENCES `emergency_contacts` (`id`),
+  ADD CONSTRAINT `emergency_student_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `internet_connection`
+--
+ALTER TABLE `internet_connection`
+  ADD CONSTRAINT `internet_connection_ibfk_1` FOREIGN KEY (`lease_id`) REFERENCES `leases` (`id`);
+
+--
+-- Constraints for table `leases`
+--
+ALTER TABLE `leases`
+  ADD CONSTRAINT `leases_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`),
+  ADD CONSTRAINT `leases_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`lease_id`) REFERENCES `leases` (`id`);
+
+--
+-- Constraints for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
